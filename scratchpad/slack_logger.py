767b3bdb5df9f3f13fd9075567a54c8c0c38cbac
c2s-pizzaseol <pizzaseol@com2us.com>
2024-10-31 10:45:21 +0900

    feat: added channel option in slack logger

--- scratchpad/slack_logger.py
+++ scratchpad/slack_logger.py
@@ -7,12 +7,15 @@ class SlackLogger(LoggerImpl):
     
     
     
-    def __init__(self, url, config: dict = None) -> None:
+    def __init__(self, url, channel: str = None, config: dict = None) -> None:
         if config is None:
             config = {}
             
         if "url" not in config:
             config["url"] = url
+
+        if channel is not None:
+            config["channel"] = channel
 
         self.config = config
             
@@ -24,9 +27,16 @@ class SlackLogger(LoggerImpl):
         headers = {
             "Content-type": "application/json"
         }
-        mergedPostFields = json.dumps({
+
+        postFields = {
             "text": json.dumps(message, indent=4)
-        }, indent=4)
+        }
+
+        channel = self.config.get("channel")
+        if channel is not None:
+            postFields["channel"] = channel
+
+        mergedPostFields = json.dumps(postFields)
         url = self.config["url"]
         
         return requests.post(url=url, headers=headers, data=mergedPostFields)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name             = 'scratchpad-py',
-    version          = '0.0.6',
+    version          = '0.0.7',
     description      = 'python logger libary',
     long_description = long_descriprion,
     long_description_content_type = "text/markdown",
